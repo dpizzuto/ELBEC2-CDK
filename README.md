73465d30443e36205301340a38c7c6b80db0ba63
dpizzuto <dpizzuto@amazon.com>
2019-11-19 10:45:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
+# EC2 - ELB deploy using Cloud Development Kit (CDK)
+The following experiment aims to show some feature of  [Cloud Development Kit](https://docs.aws.amazon.com/cdk/latest/guide/home.html). 
+Typescript is used for the example while CDK, at the date, supports: Python, JavaScript, .NET and Java. 
+
+# Goal
+Deploy Two EC2 within a private subnet of a VPC, served by a ELB.
+
+# What you need
+* Your fav IDE
+* Node.js 
+* cdk 
+* AWS Account *(if you want to deploy your stack)
+* aws-cli (configured with a role that allows to run CloudFormation stack and create resources)
+
+# Getting started
+[Here](https://docs.aws.amazon.com/cdk/latest/guide/getting_started.html) you can find the start guide to CDK.
+In case you want to start from scratch, you have to initialize a new cdk project in your favourite directory, trhough:
+
+`cdk init --language typescript`
+
+# Generate CloudFormation from Source Code
+Once you write your code following the CDK API Reference, you can generate your CloudFormation stack through a simple command:
+
+`npm run build`
+
+and then 
+
+`cdk synth`
+
+In particular the last command will produce the CloudFormation JSON within ~/youp-roject/cdk.out/prjName.template.json
+
 # Useful commands
 
  * `npm run build`   compile typescript to js
  * `npm run watch`   watch for changes and compile
  * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk deploy `      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
--- lib/elb-ec2-monitoring-stack.ts
+++ lib/elb-ec2-monitoring-stack.ts
@@ -1,9 +1,83 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require("@aws-cdk/aws-s3");
+import ec2 = require("@aws-cdk/aws-ec2");
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+import { InstanceType, UserData, SubnetType } from '@aws-cdk/aws-ec2';
 
 export class ElbEc2MonitoringStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    //Create a VPC
+    const vpc = new ec2.Vpc(this, 'ELBEC2_VPC');
+    
+    // define user data for EC2 instance: it will install HTTPD and setup a simple index.html 
+    const ud = UserData.forLinux();
+    ud.addCommands(
+      "mkdir -p /var/www/html",
+      "yum update -y",
+      "yum install -y httpd",
+      "usermod -a -G apache ec2-user",
+      "chown -R ec2-user:apache /var/www",
+      "chmod 2775 /var/www",
+      "echo \"<html> <p>Hello from Your AWSome EC2 - ElbEc2MonitoringStack</p></html>\" > /var/www/html/index.html",
+      "service httpd start");
 
-    // The code that defines your stack goes here
+      //Define a Security Group for EC2
+      const sg = new ec2.SecurityGroup(this, "ELBEC2_ec2-SG",{
+        vpc,
+        allowAllOutbound: true,
+      })
+      
+      // Define Instance 1 
+      const instance1 = new ec2.Instance(this, "myEc2Elb_1",{
+        vpc,
+        instanceType: new InstanceType("t2.micro"),
+        machineImage: new ec2.AmazonLinuxImage(),
+        keyName: "kp-ec2-cloudwatch",
+        userData: ud,
+        securityGroup: sg,
+        // availabilityZone: "eu-west-1a" //NTH
+      });
+
+      // Define Instance 2
+      const instance2 = new ec2.Instance(this, "myEc2Elb_2",{
+        vpc,
+        instanceType: new InstanceType("t2.micro"),
+        machineImage: new ec2.AmazonLinuxImage(),
+        keyName: "kp-ec2-cloudwatch",
+        userData: ud,
+        securityGroup: sg
+        // availabilityZone: "eu-west-1b" //NTH
+      });
+
+      //Define a Load Balancer and listener
+      const loadBalancer = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
+        vpc,
+        internetFacing: true
+      });
+      const lbListener = loadBalancer.addListener("listener",{
+        port: 80,
+        open: true
+      });
+      
+      //Allow Incoming traffic from Security Group of Load Balancer on EC2
+      sg.connections.allowFrom(lbListener.connections, ec2.Port.allTraffic());
+
+      //Setup Load Balancer Targets
+      const instTarget1 = new elbv2.InstanceTarget(instance1.instanceId,80);
+      const instTarget2 = new elbv2.InstanceTarget(instance2.instanceId,80);
+      lbListener.addTargets("",{
+        port: 80,
+        targets:[instTarget1, instTarget2]
+      })
+
+      //Define Load Balancer's Security Group
+      const lbSg = new ec2.SecurityGroup(this, "ELBEC2_elb-SG",{
+        vpc,
+        allowAllOutbound: true
+      })
+      lbSg.addIngressRule(ec2.Peer.anyIpv4(),ec2.Port.allTraffic());
+      lbListener.connections.addSecurityGroup(lbSg);
   }
 }
